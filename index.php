7b78a2fb19a11cea1cc9f6abf26d53e35295e097
Romain Guillemot <romain.guillemot@wildcodeschool.fr>
2020-10-06 17:08:56 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+require_once 'config.php';
+$connection = new PDO("mysql:host=" . SERVER . ";dbname=" . DATABASE . ";charset=utf8", USER, PASSWORD);
+
+$statement = $connection->query('SELECT id, title FROM recipe');
+$recipes = $statement->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>List of Recipes</title>
+    </head>
+    <body>
+        <h1>List of Recipes</h1>
+        <ul>
+            <?php foreach ($recipes as $recipe) : ?>
+            <li>
+                <a href="show.php?id=<?= $recipe['id'] ?>">
+                    <?= $recipe['title'] ?>
+                </a>
+            </li>
+            <?php endforeach ?>
+        </ul>
+    </body>
+</html>
---
+++ show.php
@@ -0,0 +1,28 @@
+<?php
+require_once 'config.php';
+$connection = new PDO("mysql:host=" . SERVER . ";dbname=" . DATABASE . ";charset=utf8", USER, PASSWORD);
+
+$query = 'SELECT title, description FROM recipe WHERE id=:id';
+$statement = $connection->prepare($query);
+$statement->bindValue(':id', $_GET['id'], PDO::PARAM_INT);
+$statement->execute();
+
+$recipe = $statement->fetch(PDO::FETCH_ASSOC);
+?>
+
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title><?= $recipe['title'] ?></title>
+    </head>
+    <body>
+        <a href="/">Home</a>
+        <h1><?= $recipe['title'] ?></h1>
+
+        <div>
+            <?= $recipe['description'] ?>
+        </div>
+    </body>
+</html>
